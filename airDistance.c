ad18ec6de59df235cb000f00f64cca2c211e64b0
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 20:12:53 +0500

    give prompt to userfor input

--- airDistance.c
+++ airDistance.c
@@ -4,5 +4,10 @@ int main(){
     double latorigin,latdestination,longorigin,longdestination;
     double airdistance, delta;
     const float radiouearth= 6371;
+    printf("\t\t Enter the value in degree\t\t\n");
+    printf("Enter the value of Latitude and Longtitude of Origin(Latitude,longtitude):");
+    scanf("%lf,%lf",&latorigin,&longorigin);
+    printf("Enter the value of Latitude and Longtitude of Destination(latitude ,longtitude):");
+    scanf("%lf,%lf",&latdestination,&longdestination);
     
 }
